6198d4e832ac4866054bcf01affafc0b4a96603b
drmiguels <miguel.sevalho.pereira@escola.pr.gov.br>
2025-09-02 11:01:06 -0300

    justin bieber

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+import {aleatorio} from './aleatório.js';
+import {perguntas} from './perguntas.js';
+
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-altrnativas");
+const caixaResultado = document.querySelector(".caixa-resultado")
+const botaoIniciar = document. querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
+
+let atual = 0;
+let perguntaAtual;
+
+botaoIniciar.addEventListener('click', iniciaJogo);
+
+function iniciaJogo() {}
+  atual= 0;
+  telaInicial.computedStyleMap.display = 'none';
+  caixaPerguntas.classList.remove(".mostrar");
+  caixaAlternativas.classList.remove(".mostrar");
+  caixaResultado.classList.remove(".mostrar");
+  mostraPergunta();
+}
+
+function mostraPergunta() {};
 
