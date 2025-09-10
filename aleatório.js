fdada4c87aaabf1bd19d1ad20f5f6f649b38f9f0
drmiguels <miguel.sevalho.pereira@escola.pr.gov.br>
2025-09-10 07:33:54 -0300

    wtf

--- aleatório.js
+++ aleatório.js
@@ -1,5 +1,8 @@
 const nomes = ["Hércules","Evandro","P.didy","Atenas","Afrodite","Lívia"]
 
 function aleatório(lista){
-    const posicao = Math.floor(Math.random )
+    const posicao = Math.floor(Math.random ()* lista.lenght);
+    return lista[posicao];
 }
+
+export
