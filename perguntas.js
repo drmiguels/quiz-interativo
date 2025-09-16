c50a0608aeeb397a49b2f631823ecffa3cee1ad5
drmiguels <miguel.sevalho.pereira@escola.pr.gov.br>
2025-09-16 11:20:21 -0300

    que isso meu filho calma

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,19 @@
 const perguntas = [
     {
-        enunciado: "Pergunta 1", 
+        enunciado: "você prefere harley ou big trail???", 
         alternativas: [
             {
-                texto: "alternativa 1",
+                texto: " Harley Davidson",
                 afirmacao: [
-                    "afirmação 1",
+                    "Ès um homem de respeito",
                     "afirmação 2"
                 ],
                 proxima: 1,
             },
             {
-                texto: "alternativa 2",
+                texto: "Big trail(BMW)",
                 afirmaca o: [
-                    "afirmação 1",
+                    " Sua mãe tá sabendo que você gosta de rapazes??",
                     "afirmação 2"
                 ],
                 proxima: 1,
@@ -21,40 +21,40 @@ const perguntas = [
         ]
     },
         {
-        enunciado: "Pergunta 2", 
+        enunciado: " você está perdido em uma estrada distante á noite, pois sua moto quebrou, preferes buscar ajuda na estrada ou tentar conserta-la ? ", 
         alternativas: [
             {
-                texto: "alternativa 1",
+                texto: " buscar ajuda ",
                 afirmacao: [
-                    "afirmação 1",
+                    " não consegue avistar nenhum automóvel ",
                     "afirmação 2"
                 ],
                 proxima: 2,
             },
             {
-                texto: "alternativa 2",
+                texto: "consertar a moto",
                 afirmaca o: [
-                    "afirmação 1",
+                    " não consegue pois as peças se espalharam pela trilha da floresta",
                     "afirmação 2"
                 ],
             }
         ]
     },
         {
-        enunciado: "Pergunta 3", 
+        enunciado: " Embora seja perigoso, você decide ir a trilha da floresta para procurar as peças da sua moto, porém se depara com um urso", 
         alternativas: [
             {
-                texto: "alternativa 1",
+                texto: " você enfrenta ",
                 afirmacao: [
-                    "afirmação 1",
+                    " você saca a sua faca do bolso e crava com muita prescisão no crânio do urso ",
                     "afirmação 2"
                 ],
             },
             proxima: 3,
             {
-                texto: "alternativa 2",
+                texto: " você tenta fugir",
                 afirmaca o: [
-                    "afirmação 1",
+                    " não tem muito oque c fazer, mas você vai acabar morto",
                     "afirmação 2"
                 ],
                 proxima: 3,
@@ -62,27 +62,27 @@ const perguntas = [
         ]
     },
         {
-        enunciado: "Pergunta 4", 
+        enunciado: " Por sorte, no caminho você acha uma lanterna, você abre o urso para procurar peças ou tenta ir atrás de sua moto?", 
         alternativas: [
             {
-                texto: "alternativa 1",
+                texto: " abrir o urso",
                 afirmacao: [
-                    "afirmação 1",
+                    " você acha uma chave de fenda",
                     "afirmação 2"
                 ],
             },
             proxima: 4,
             {
-                texto: "alternativa 2",
+                texto: " voltar para a estrada ",
                 afirmaca o: [
-                    "afirmação 1",
+                    " medroso",
                     "afirmação 2"
                 ],
             }
         ]
     },
         {
-        enunciado: "Pergunta 5", 
+        enunciado: " Quando volta a estrada já amanheceu, ", 
         alternativas: [
             {
                 texto: "alternativa 1",
